09ec96b7d0e7bac8d7ec8b9d4d745b2191b9692c
David Oke <davidokenyc@gmail.com>
2026-03-31 18:24:20 -0400

    add duckdb database as passthrough

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,5 @@
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("styles.css");
   eleventyConfig.addPassthroughCopy("assets");
+  eleventyConfig.addPassthroughCopy("src/report_card.duckdb");
 };
